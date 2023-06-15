ba1db553dbf708685b9c561a7e2b3fd42cc2b769
Andrew4sure <abe3andrew@gmail.com>
2023-06-15 16:14:24 -0700

    Preprocessor

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-
+include < stdio.h >
+/*
+ * write a program that prints
+ *
+ *return: always 0 (successs)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,"\n);
+	return (0);
+}
