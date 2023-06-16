ce85f46d5a23cef074b974f175c6236a44c03819
Andrew4sure <abe3andrew@gmail.com>
2023-06-15 17:09:34 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,9 @@
  *
  * Return: always 0(success)
  */
-int main(void)
+int main(void);
 {
-puts(""\Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 
-return(0)
+return (0);
 }
