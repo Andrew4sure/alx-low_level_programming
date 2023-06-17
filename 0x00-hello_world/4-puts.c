1addf157b60e1717a7c48f4f179c7cecdfa2ca65
Andrew4sure <abe3andrew@gmail.com>
2023-06-17 05:58:54 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  *
  * Return: Always 0(success)
  */
-int main(void);  /* Using puts function only */
+Int main(void)    /* Using puts function only */
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
