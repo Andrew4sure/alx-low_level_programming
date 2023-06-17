47c0d59d1bffef98e58c433e9dc8c43c172be8f7
Andrew4sure <abe3andrew@gmail.com>
2023-06-17 06:08:08 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /*
- * main-Entry point
+ * main function uses puts only to print information
  *
  * Return: Always 0(success)
  */
-Int main(void)    /* Using puts function only */
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle"\n);
 	return (0);
 }
