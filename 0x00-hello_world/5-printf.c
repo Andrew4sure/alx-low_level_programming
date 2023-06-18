13d56ff3521e39f68bfd217dff1ccc9e76939e54
Andrew4sure <abe3andrew@gmail.com>
2023-06-17 23:47:51 -0700

    Size is not grandeur

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
- * Main - Entry point
- * program that prints Using the function printf
+ * main -Entry point
  *
- * Return: Always 0 (success)
+ * Returns: Always 0 (success)
  */
-
+/*
+ * description: Program with printf
+ */
 int main(void)
-
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,12 @@
  *
  * Return: Always 0 (success)
  */
-int main (void)
+int main(void)
 {
-	printf(sizeof char%d, char)
-	printf(sizeof int
+	printf(Size of a char : "sizeof %d\n" byte(s), char);
+	printf(Size of an int : "sizeof %d\n" byte(s), int);
+	printf(Size of a long int : "sizeof %d\n" byte(s), long int);
+	printf(Size of a long long int : "sizeof %d\n" byte(s), long long int);
+	printf(Size of a float : "sizeof %d\n" byte(s), float);
+	return (0);
+}
